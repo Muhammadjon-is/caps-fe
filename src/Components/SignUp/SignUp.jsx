cce26a9f9ba013788c72faad8c3326b6c5eed529
Muhammadjon-is <isakovw18@gmail.com>
2023-03-09 00:13:51 +0100

    Update SignUp.jsx

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -3,8 +3,35 @@ import Crumb from "../Login.jsx/Crumb";
 import Meta from "../Login.jsx/Meta";
 import LoginContainer from "../Login.jsx/LoginContainer";
 import LoginInput from "../Login.jsx/LoginIput";
-import "./SignUp.css"
-const SignUp = () => {
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { register } from "../Redux/Actions/userAction";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../LoadingError/Error";
+import Loading from "../LoadingError/Loading";
+import "./SignUp.css";
+const SignUp = ({ history }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const redirect = useParams();
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { error, loading, userInfo } = userRegister;
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [userInfo, history, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(register(name, email, password));
+  };
+
   return (
     <>
       <Meta title={"Sign Up"} />
@@ -14,18 +41,41 @@ const SignUp = () => {
           <div className="col-12">
             <div className="auth-card">
               <h3 className="text-center mb-3">Sign Up</h3>
-              <form action="" className="d-flex flex-column gap-15">
-                <LoginInput type="text" name="name" placeholder="Name" />
-                <LoginInput type="email" name="email" placeholder="Email" />
-               
+              {error && <Message variant="alert-danger">{error}</Message>}
+              {loading && <Loading />}
+
+              <form
+                action=""
+                className="d-flex flex-column gap-15"
+                onSubmit={submitHandler}
+              >
+                <LoginInput
+                  type="text"
+                  placeholder="Username"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <LoginInput
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
                 <LoginInput
                   type="password"
-                  name="password"
                   placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
                 <div>
                   <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
                     <button className="button border-0">Sign Up</button>
+                    <p>
+            <Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
+              I Have Account <strong>Login</strong>
+            </Link>
+          </p>
                   </div>
                 </div>
               </form>
