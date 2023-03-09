69dd58749ab979201e0e074783a6a70c8c4d951e
Muhammadjon-is <isakovw18@gmail.com>
2023-03-09 01:58:21 +0100

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import {  Link } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 
 import "./Header.css"
 const Header = () => {
+  const [keyword, setKeyword] = useState()
   return (
     <>
     
