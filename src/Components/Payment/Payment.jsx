8559b178a6c1cbaa52c6b782d4e2a75f072e522e
Muhammadjon-is <isakovw18@gmail.com>
2023-03-11 10:13:07 +0100

    Payment Section

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { savePaymentMethod } from '../Redux/Actions/cartAction'
 
-const Payment = () => {
+
+const Payment = ({history}) => {
+
+    const cart = useSelector((state) => state.cart);
+    const {shippingAddress} = cart;
+   
+    if(!shippingAddress){
+        history.push("/shipping");
+    }
+
+    const [paymentMethod, setPaymentMethod] = useState("Paypal")
+
+    const dispatch = useDispatch()
+
+    const submitHandler = (e) => {
+        e.prevenetDefault();
+        dispatch(savePaymentMethod(paymentMethod));
+        history.push("/placeorder")
+    }
+  
   return (
-    <div>Payment</div>
+    <div className="container d-flex justify-content-center align-items-center login-center">
+        <form
+          className="Login2 col-md-8 col-lg-4 col-11"
+          onSubmit={submitHandler}
+        >
+          <h6>SELECT PAYMENT METHOD</h6>
+          <div className="payment-container">
+            <div className="radio-container">
+              <input
+                className="form-check-input"
+                type="radio"
+                value={paymentMethod}
+                onChange={(e) => setPaymentMethod(e.target.value)}
+              />
+              <label className="form-check-label">PayPal or Credit Card</label>
+            </div>
+          </div>
+
+          <button type="submit">Continue</button>
+        </form>
+      </div>
   )
 }
 
