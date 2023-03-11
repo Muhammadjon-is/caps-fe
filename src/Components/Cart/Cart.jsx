00af4a6a0e87bcdbb254ae8f31fe5340d9b140ba
Muhammadjon-is <isakovw18@gmail.com>
2023-03-11 10:25:40 +0100

    Update Cart.jsx

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,71 +1,127 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React,{useEffect} from "react";
+import { Link, useParams } from "react-router-dom";
 import LoginContainer from "../Login.jsx/LoginContainer";
 import Crumb from "../Login.jsx/Crumb";
 import Meta from "../Login.jsx/Meta";
-import images from "../../assets/gamingPc1.png"
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removefromcart } from "../Redux/Actions/cartAction";
+// import images from "../../assets/gamingPc1.png"
+
+
+
 import "./Cart.css"
-const Cart = () => {
+const Cart = ({history}) => {
+
+
+  const dispatch = useDispatch();
+  const productId = useParams()
+  const qty = useParams()
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const total = cartItems.reduce((a, i) => a + i.qty * i.price, 0).toFixed(2);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  const checkOutHandler = () => {
+    history.push("/login?redirect=shipping");
+  };
+
+  const removeFromCartHandle = (id) => {
+    dispatch(removefromcart(id));
+  };
+
+
   return (
     <>
       <Meta title={"Cart"} />
       <Crumb title="MyCart" />
       <LoginContainer class1="cart-wrapper home-wrapper-2 py-5">
-        <div className="row">
-          <div className="col-12">
-            <div className="cart-header cart-wrapper py-3 d-flex justify-content-between align-items-center">
-              <h4 className="cart-col-1">Product</h4>
-              <h4 className="cart-col-2">Price</h4>
-              <h4 className="cart-col-3">Quantity</h4>
-              <h4 className="cart-col-4">Total</h4>
-            </div>
-            <div className="cart-data py-3 mb-2 d-flex justify-content-between align-items-center">
-              <div className="cart-col-1 gap-15 d-flex align-items-center">
-                <div className="w-25">
-                  <img src={images} className="img-fluid" alt="product image" />
-                </div>
-                <div className="w-75">
-                  <p>Model:Asus</p>
-                  <p>Color: gfd</p>
-                </div>
-              </div>
-              <div className="cart-col-2">
-                <h5 className="price">$ 100</h5>
-              </div>
-              <div className="cart-col-3 d-flex align-items-center gap-15">
-                <div>
-                  <input
-                    className="form-control"
-                    type="number"
-                    name=""
-                    min={1}
-                    max={10}
-                    id=""
-                  />
-                </div>
-                <div>
-                </div>
-              </div>
-              <div className="cart-col-4">
-                <h5 className="price">$ 100</h5>
-              </div>
-            </div>
+      <div className="container">
+        {cartItems.length === 0 ? (
+          <div className=" alert alert-info text-center mt-3">
+            Your cart is empty
+            <Link
+              className="btn btn-success mx-5 px-5 py-3"
+              to="/"
+              style={{
+                fontSize: "12px",
+              }}
+            >
+              SHOPPING NOW
+            </Link>
           </div>
-          <div className="col-12 py-2 mt-4">
-            <div className="d-flex justify-content-between align-items-baseline">
-              <Link to="/OurStore" className="button">
-                Continue To Shopping
+        ) : (
+          <>
+            <div className=" alert alert-info text-center mt-3">
+              Total Cart Products
+              <Link className="text-success mx-2" to="/cart">
+                ({cartItems.length})
               </Link>
-              <div className="d-flex flex-column align-items-end">
-                <h4>SubTotal: $ 1000</h4>
-                <p>Taxes and shipping calculated at checkout</p>
-                <Link to="/checkout" className="button">
-                  Checkout
-                </Link>
-              </div>
             </div>
-          </div>
-        </div>
+            {/* cartiterm */}
+            {cartItems.map((item) => (
+              <div className="cart-iterm row">
+                <div
+                  onClick={() => removeFromCartHandle(item.product)}
+                  className="remove-button d-flex justify-content-center align-items-center"
+                >
+                  <i className="fas fa-times"></i>
+                </div>
+                <div className="cart-image col-md-3">
+                  <img src={item.image} alt={item.name} />
+                </div>
+                <div className="cart-text col-md-5 d-flex align-items-center">
+                  <Link to={`/products/${item.product}`}>
+                    <h4>{item.name}</h4>
+                  </Link>
+                </div>
+                <div className="cart-qty col-md-2 col-sm-5 mt-md-5 mt-3 mt-md-0 d-flex flex-column justify-content-center">
+                  <h6>QUANTITY</h6>
+                  <select
+                    value={item.qty}
+                    onChange={(e) =>
+                      dispatch(addToCart(item.product, Number(e.target.value)))
+                    }
+                  >
+                    {[...Array(item.countInStock).keys()].map((x) => (
+                      <option key={x + 1} value={x + 1}>
+                        {x + 1}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="cart-price mt-3 mt-md-0 col-md-2 align-items-sm-end align-items-start  d-flex flex-column justify-content-center col-sm-7">
+                  <h6>PRICE</h6>
+                  <h4>${item.price}</h4>
+                </div>
+              </div>
+            ))}
+
+            {/* End of cart iterms */}
+            <div className="total">
+              <span className="sub">total:</span>
+              <span className="total-price">${total}</span>
+            </div>
+            <hr />
+            <div className="cart-buttons d-flex align-items-center row">
+              <Link to="/" className="col-md-6 ">
+                <button>Continue To Shopping</button>
+              </Link>
+              {total > 0 && (
+                <div className="col-md-6 d-flex justify-content-md-end mt-3 mt-md-0">
+                  <button onClick={checkOutHandler}>Checkout</button>
+                </div>
+              )}
+            </div>
+          </>
+        )}
+      </div>
       </LoginContainer>
     </>
   );
