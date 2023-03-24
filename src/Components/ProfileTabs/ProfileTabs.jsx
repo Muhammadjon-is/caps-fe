37ed61bddfaa10f3324d5618aba51e1ce5f44133
Muhammadjon-is <isakovw18@gmail.com>
2023-03-24 13:25:28 +0100

    Toastify fixed

--- src/Components/ProfileTabs/ProfileTabs.jsx
+++ src/Components/ProfileTabs/ProfileTabs.jsx
@@ -3,24 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 import Message from "../LoadingError/Error";
 import Toast from "./../LoadingError/Toast"
 import Loading from "./../LoadingError/Loading";
-import {  ToastContainer } from "react-toastify";
-// import { ToastContainer  } from "react-toastify";
- import { updateUserProfile } from "../Redux/Actions/userAction";
-const toast = ToastContainer
+import {  toast } from 'react-toastify';
+import { updateUserProfile } from "../Redux/Actions/userAction";
+import 'react-toastify/dist/ReactToastify.css';
 
 const ProfileTabs = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const toastId = React.useRef(null);
-
-  const Toastobjects = {
-    pauseOnFocusLoss: false,
-    draggable: false,
-    pauseOnHover: false,
-    autoClose: 2000,
-  };
+ 
 
   const dispatch = useDispatch();
 
@@ -41,74 +33,73 @@ const ProfileTabs = () => {
     e.preventDefault();
     // Password match
     if (password !== confirmPassword) {
-      if (!toast.isActive(toastId.current)) {
-        toastId.current = toast.error("Password does not match", Toastobjects);
-      }
+      toast.error('Password does not match !!', {
+        position: toast.POSITION.TOP_RIGHT
+    });
     } else {
       dispatch(updateUserProfile({ id: user._id, name, email, password }));
-      if (!toast.isActive(toastId.current)) {
-        toastId.current = toast.success("Profile Updated", Toastobjects);
-      }
+      toast.error('Success Notification !', {
+        position: toast.POSITION.TOP_RIGHT
+    });
     }
   };
   return (
     <>
-      <Toast />
-      {error && <Message variant="alert-danger">{error}</Message>}
-      {loading && <Loading />}
-      {updateLoading && <Loading />}
-      <form className="row  form-container" onSubmit={submitHandler}>
-        <div className="col-md-6">
-          <div className="form">
-            <label for="account-fn">UserName</label>
-            <input
-              className="form-control"
-              type="text"
-              required
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
+    <Toast />
+    {error && <Message variant="alert-danger">{error}</Message>}
+    {loading && <Loading />}
+    {updateLoading && <Loading />}
+    <form className="row  form-container" onSubmit={submitHandler}>
+      <div className="col-md-6">
+        <div className="form">
+          <label for="account-fn">UserName</label>
+          <input
+            className="form-control"
+            type="text"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
+      </div>
 
-        <div className="col-md-6">
-          <div className="form">
-            <label for="account-email">E-mail Address</label>
-            <input
-              className="form-control"
-              type="email"
-              value={email}
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+      <div className="col-md-6">
+        <div className="form">
+          <label for="account-email">E-mail Address</label>
+          <input
+            className="form-control"
+            type="email"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <div className="col-md-6">
-          <div className="form">
-            <label for="account-pass">New Password</label>
-            <input
-              className="form-control"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+      </div>
+      <div className="col-md-6">
+        <div className="form">
+          <label for="account-pass">New Password</label>
+          <input
+            className="form-control"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <div className="col-md-6">
-          <div className="form">
-            <label for="account-confirm-pass">Confirm Password</label>
-            <input
-              className="form-control"
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
+      </div>
+      <div className="col-md-6">
+        <div className="form">
+          <label for="account-confirm-pass">Confirm Password</label>
+          <input
+            className="form-control"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
         </div>
-        <button type="submit">Update Profile</button>
-        <ToastContainer/>
-      </form>
-    </>
+      </div>
+      <button type="submit">Update Profile</button>
+    </form>
+  </>
   );
 };
 
